7c30724e98faba0dae578b26da68729807020eac
Davut Sukhankuliev <stalker.davut2001@gmail.com>
2024-01-13 02:04:45 +0300

    tasks 1,2 lesson 3 complete

--- Lesson 3 - Branching algorithms/Program.cs
+++ Lesson 3 - Branching algorithms/Program.cs
@@ -8,8 +8,29 @@ namespace Lesson_3___Branching_algorithms
         {
             /*Обязательная задача*. Ввести целочисленные координаты трех вершин прямоугольника,
              стороны которого параллельны координатным осям. Найти координаты его четвертой вершины.
-             Если пользователь введёт координаты точек так, что нельзя получить прямоугольник со сторонами,
+             Если пользователь введёт координаты точек так, что нельзя получить прямоугольник со сторонами, 
              параллельными координатным осям, вывести соответствующее сообщение.*/
+            Console.WriteLine("Введите 2D координаты первой точки через запятую:");
+            var strings = Console.ReadLine().Split(new char[]{','},StringSplitOptions.RemoveEmptyEntries);
+            var pointA = new Point2D(int.Parse(strings[0]), int.Parse(strings[1]));
+            
+            Console.WriteLine("Введите 2D координаты второй точки через запятую:");
+            strings = Console.ReadLine().Split(new char[]{','},StringSplitOptions.RemoveEmptyEntries);
+            var pointB = new Point2D(int.Parse(strings[0]), int.Parse(strings[1]));
+            
+            Console.WriteLine("Введите 2D координаты третьей точки через запятую:");
+            strings = Console.ReadLine().Split(new char[]{','},StringSplitOptions.RemoveEmptyEntries);
+            var pointC = new Point2D(int.Parse(strings[0]), int.Parse(strings[1]));
+
+            //Task 1
+            /*var pointD = Task1.FindFourthCoord(pointA, pointB, pointC);
+            Console.WriteLine($"pointD: {pointD.X},{pointD.Y}");*/
+            
+            //Task 2
+            Point2D point;
+            Console.WriteLine(!Task2.Closest(pointA, pointB, pointC, out point)
+                ? $"Точки B и C равноудалены от точки A\r\nКоордината точки: {point.Text()}"
+                : $"Координата ближайшей точки: {point.Text()}");
         }
     }
 }
--- Lesson 3 - Branching algorithms/Task1.cs
+++ Lesson 3 - Branching algorithms/Task1.cs
@@ -2,43 +2,44 @@
 
 namespace Lesson_3___Branching_algorithms
 {
-    struct Point2D
+    /*Обязательная задача*. Ввести целочисленные координаты трех вершин прямоугольника,
+     стороны которого параллельны координатным осям. Найти координаты его четвертой вершины.
+     Если пользователь введёт координаты точек так, что нельзя получить прямоугольник со сторонами,
+     параллельными координатным осям, вывести соответствующее сообщение.*/
+    internal struct Point2D
     {
-        internal int x;
-        internal int y;
-    }
-    
-    class Task1
-    {
-        void Main()
+        private int _x;
+        private int _y;
+
+        public int X => _x;
+        public int Y => _y;
+        
+        public Point2D(int x, int y)
         {
-            /*Обязательная задача*. Ввести целочисленные координаты трех вершин прямоугольника,
-             стороны которого параллельны координатным осям. Найти координаты его четвертой вершины.
-             Если пользователь введёт координаты точек так, что нельзя получить прямоугольник со сторонами, 
-             параллельными координатным осям, вывести соответствующее сообщение.*/
-            var rectangle = new Point2D[4];
-                
-            for (int i = 1; i <= 6; i++)
+            _x = x;
+            _y = y;
+        }
+
+        public string Text()
+        {
+            return $"{_x},{_y}";
+        }
+    }
+
+    internal class Task1
+    {
+        public static Point2D FindFourthCoord(Point2D pointA, Point2D pointB, Point2D pointC)
+        {
+            if (pointA.X == pointB.X && pointB.Y == pointC.Y)
             {
-                string axis = "x";
-                int number = 1;
-                
-                if (i % 2 == 0)
-                {
-                    axis = "y";
-                    number /= 2;
-                }
-
-                Console.WriteLine($"Введите координату {axis}{number} прямоугольника");
-
-                if (i % 2 != 0)
-                {
-                    rectangle[number].x = int.Parse(Console.ReadLine() ?? string.Empty);
-                }
-                else
-                {
-                    rectangle[number].y = int.Parse(Console.ReadLine() ?? string.Empty);
-                }
+                var x = pointA.X + pointC.X - pointB.X;
+                var y = pointA.Y + pointC.Y - pointB.Y;
+                return new Point2D(x, y);
+            }
+            else
+            {
+                Console.WriteLine("Введенные точки не могут образовать прямоугольник со сторонами, параллельными координатным осям.");
+                return new Point2D();
             }
         }
     }
