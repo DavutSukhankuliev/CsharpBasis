aeedabe3823f837386d4e645e508d1a700cb3787
Davut Sukhankuliev <stalker.davut2001@gmail.com>
2024-01-14 14:39:32 +0300

    lesson 3 tasks complete

--- Lesson 3 - Branching algorithms/Program.cs
+++ Lesson 3 - Branching algorithms/Program.cs
@@ -6,11 +6,8 @@ namespace Lesson_3___Branching_algorithms
     {
         public static void Main(string[] args)
         {
-            /*Обязательная задача*. Ввести целочисленные координаты трех вершин прямоугольника,
-             стороны которого параллельны координатным осям. Найти координаты его четвертой вершины.
-             Если пользователь введёт координаты точек так, что нельзя получить прямоугольник со сторонами, 
-             параллельными координатным осям, вывести соответствующее сообщение.*/
-            Console.WriteLine("Введите 2D координаты первой точки через запятую:");
+            //Task 1-2
+            /*Console.WriteLine("Введите 2D координаты первой точки через запятую:");
             var strings = Console.ReadLine().Split(new char[]{','},StringSplitOptions.RemoveEmptyEntries);
             var pointA = new Point2D(int.Parse(strings[0]), int.Parse(strings[1]));
             
@@ -20,17 +17,39 @@ namespace Lesson_3___Branching_algorithms
             
             Console.WriteLine("Введите 2D координаты третьей точки через запятую:");
             strings = Console.ReadLine().Split(new char[]{','},StringSplitOptions.RemoveEmptyEntries);
-            var pointC = new Point2D(int.Parse(strings[0]), int.Parse(strings[1]));
+            var pointC = new Point2D(int.Parse(strings[0]), int.Parse(strings[1]));*/
 
             //Task 1
             /*var pointD = Task1.FindFourthCoord(pointA, pointB, pointC);
             Console.WriteLine($"pointD: {pointD.X},{pointD.Y}");*/
             
             //Task 2
-            Point2D point;
+            /*Point2D point;
             Console.WriteLine(!Task2.Closest(pointA, pointB, pointC, out point)
                 ? $"Точки B и C равноудалены от точки A\r\nКоордината точки: {point.Text()}"
-                : $"Координата ближайшей точки: {point.Text()}");
+                : $"Координата ближайшей точки: {point.Text()}");*/
+            
+            //Task 3
+            /*Console.WriteLine("Введите число от 20 до 69");
+            if (int.TryParse(Console.ReadLine(), out var age))
+            {
+                Console.WriteLine(Task3.AgeToString(age));
+            }
+            else
+            {
+                Console.WriteLine("Введите число");
+            }*/
+            
+            //Task 4
+            Console.WriteLine("Введите число от 100 до 1000");
+            if (int.TryParse(Console.ReadLine(), out var number))
+            {
+                Console.WriteLine(Task4.IntToString(number));
+            }
+            else
+            {
+                Console.WriteLine("Введите число");
+            }
         }
     }
 }
