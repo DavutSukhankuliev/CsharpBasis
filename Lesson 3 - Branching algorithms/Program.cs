babab40a123f97b11f20244a0114a996aa398093
Davut Sukhankuliev <stalker.davut2001@gmail.com>
2024-01-21 01:32:24 +0300

    lesson 3 task choose option added

--- Lesson 3 - Branching algorithms/Program.cs
+++ Lesson 3 - Branching algorithms/Program.cs
@@ -6,50 +6,78 @@ namespace Lesson_3___Branching_algorithms
     {
         public static void Main(string[] args)
         {
-            //Task 1-2
-            /*Console.WriteLine("Введите 2D координаты первой точки через запятую:");
-            var strings = Console.ReadLine().Split(new char[]{','},StringSplitOptions.RemoveEmptyEntries);
-            var pointA = new Point2D(int.Parse(strings[0]), int.Parse(strings[1]));
+            int taskNumber;
+            do
+            {
+                Console.WriteLine("Введите номер задачи 1-4. Для выполнения нажмите Enter");
+                int.TryParse(Console.ReadLine(), out taskNumber);
+            } while (taskNumber < 1 || taskNumber > 4);
             
-            Console.WriteLine("Введите 2D координаты второй точки через запятую:");
-            strings = Console.ReadLine().Split(new char[]{','},StringSplitOptions.RemoveEmptyEntries);
-            var pointB = new Point2D(int.Parse(strings[0]), int.Parse(strings[1]));
+            string result;
+            switch (taskNumber)
+            {
+                case 1:
+                case 2:
+                {
+                    
+                    // TODO: loop and case 1 and 2 if statement fix
+                    
+                    Console.WriteLine("Введите 2D координаты первой точки через запятую, по типу 'x,y':");
+                    var strings = Console.ReadLine()?.Split(new char[]{','},StringSplitOptions.RemoveEmptyEntries);
+                    var pointA = new Point2D(int.Parse(strings?[0] ?? string.Empty), int.Parse(strings?[1] ?? string.Empty));
             
-            Console.WriteLine("Введите 2D координаты третьей точки через запятую:");
-            strings = Console.ReadLine().Split(new char[]{','},StringSplitOptions.RemoveEmptyEntries);
-            var pointC = new Point2D(int.Parse(strings[0]), int.Parse(strings[1]));*/
+                    Console.WriteLine("Введите 2D координаты второй точки через запятую:");
+                    strings = Console.ReadLine()?.Split(new char[]{','},StringSplitOptions.RemoveEmptyEntries);
+                    var pointB = new Point2D(int.Parse(strings?[0] ?? string.Empty), int.Parse(strings?[1] ?? string.Empty));
+            
+                    Console.WriteLine("Введите 2D координаты третьей точки через запятую:");
+                    strings = Console.ReadLine()?.Split(new char[]{','},StringSplitOptions.RemoveEmptyEntries);
+                    var pointC = new Point2D(int.Parse(strings?[0] ?? string.Empty), int.Parse(strings?[1] ?? string.Empty));
 
-            //Task 1
-            /*var pointD = Task1.FindFourthCoord(pointA, pointB, pointC);
-            Console.WriteLine($"pointD: {pointD.X},{pointD.Y}");*/
-            
-            //Task 2
-            /*Point2D point;
-            Console.WriteLine(!Task2.Closest(pointA, pointB, pointC, out point)
-                ? $"Точки B и C равноудалены от точки A\r\nКоордината точки: {point.Text()}"
-                : $"Координата ближайшей точки: {point.Text()}");*/
-            
-            //Task 3
-            /*Console.WriteLine("Введите число от 20 до 69");
-            if (int.TryParse(Console.ReadLine(), out var age))
-            {
-                Console.WriteLine(Task3.AgeToString(age));
+                    if (taskNumber == 1)
+                    {
+                        var pointD = Task1.FindFourthCoord(pointA, pointB, pointC);
+                        result = $"Кооридинаты 4ой точки прямоугольника: {pointD.X},{pointD.Y}";
+                    }
+                    else
+                    {
+                        result = !Task2.Closest(pointA, pointB, pointC, out var point)
+                            ? $"Точки B и C равноудалены от точки A\r\nКоордината точки: {point.Text()}"
+                            : $"Координата ближайшей точки: {point.Text()}";
+                    }
+                    
+                    break;
+                }
+
+                case 3:
+                {
+                    Console.WriteLine("Введите число от 20 до 69");
+                    result = int.TryParse(Console.ReadLine(), out var age)
+                        ? Task3.AgeToString(age)
+                        : "Введите число";
+
+                    break;
+                }
+
+                case 4:
+                {
+                    Console.WriteLine("Введите число от 100 до 1000");
+                    result = int.TryParse(Console.ReadLine(), out var number)
+                        ? Task4.IntToString(number)
+                        : "Введите число";
+
+                    break;
+                }
+
+                default:
+                {
+                    result = string.Empty;
+                    
+                    break;
+                }
             }
-            else
-            {
-                Console.WriteLine("Введите число");
-            }*/
-            
-            //Task 4
-            Console.WriteLine("Введите число от 100 до 1000");
-            if (int.TryParse(Console.ReadLine(), out var number))
-            {
-                Console.WriteLine(Task4.IntToString(number));
-            }
-            else
-            {
-                Console.WriteLine("Введите число");
-            }
+
+            Console.WriteLine(result);
         }
     }
 }
