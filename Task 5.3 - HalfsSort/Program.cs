6720a95cfac2ccb54f6cd02f8e5bccc4333e223a
Davut Sukhankuliev <stalker.davut2001@gmail.com>
2024-02-04 01:20:52 +0300

    tasks 5.1-5.2 complete

---
+++ Task 5.3 - HalfsSort/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Task_5._3___HalfsSort
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var random = new Random();
+            var array = new int[10];
+            for (var i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(-50, 51);
+                array[i] += 50;
+                Console.Write($"{array[i]} ");
+            }
+
+            Console.WriteLine();
+            var result = SortHalfsNorm(array, 100);
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] -= 50;
+                Console.Write($"{array[i]} ");
+            }
+        }
+
+        private static int[] SortHalfsNorm(int[] array, int maxValue)
+        {
+            /*for (int i = 0; i < arr.Length; i++)
+            {
+                if (i < arr.Length / 2)
+                {
+                    
+                }
+                else
+                {
+                    
+                }
+            }*/
+            
+            int[] counts = new int[maxValue + 1];
+
+            foreach (int num in array)
+            {
+                counts[num]++;
+            }
+
+            int index = 0;
+            for (int i = 0; i < counts.Length; i++)
+            {
+                for (int j = 0; j < counts[i]; j++)
+                {
+                    array[index++] = i;
+                }
+            }
+
+            return array;
+        }
+    }
+}
