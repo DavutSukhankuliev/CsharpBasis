626111dca3ad10a591f8af63c8d67ebe07dd4227
Davut Sukhankuliev <stalker.davut2001@gmail.com>
2024-02-17 20:30:36 +0300

    6.2 complete

---
+++ Lesson 6 - Strings/Task 6.2 - IsPalindrome/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_6._2___IsPalindrome
+{
+    /*Обязательная задача*. Ввести с клавиатуры предложение. Предложение представляет собой слова, разделенные пробелом.
+     Знаки препинания не используются. Составить программу, определяющую является ли строка палиндромом без учёта 
+     пробелов и регистра (пример палиндрома – «А роза упала на лапу Азора»).*/
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string sentence = Console.ReadLine();
+            Console.WriteLine($"Данное предложение является палиндромом: {sentence.IsPalindrome()}");
+        }
+
+        private static bool IsPalindrome(this string sentence)
+        {
+            string cacheString = new string(sentence.ToLower().Replace(" ", "").ToCharArray());
+            string newString = "";
+            
+            foreach (char c in cacheString)
+            {
+                newString = c + newString;
+            }
+            
+            return cacheString.Equals(newString);
+        }
+    }
+}
---
+++ Lesson 6 - Strings/Task 6.2 - IsPalindrome/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Task_6._2___IsPalindrom")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task_6._2___IsPalindrom")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("88FDCE3A-E3FC-4027-B8FC-3A3AF33CAAB7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
