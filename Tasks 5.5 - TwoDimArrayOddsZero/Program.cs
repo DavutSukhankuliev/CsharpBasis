47f9911bc5662b07c0889228403c685bfef7e883
Davut Sukhankuliev <stalker.davut2001@gmail.com>
2024-02-17 14:21:34 +0300

    5.5 complete

---
+++ Tasks 5.5 - TwoDimArrayOddsZero/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Tasks_5._5___TwoDimArrayOddsZero
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+            do
+            {
+                Console.WriteLine("Впишите целочисленное значение N для формирования двумерного массива NxN");
+            } while (!int.TryParse(Console.ReadLine(), out n));
+
+            var array = Array2D(n);
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"{array[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static int[,] Array2D(int size)
+        {
+            int[,] newArray = new int[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    newArray[i, j] = (i + j) % 2 == 0 ? 1 : 0;
+                }
+            }
+
+            return newArray;
+        }
+    }
+}
