e7a62d51874d5a4103784761467c34b8df15c4a5
Davut Sukhankuliev <stalker.davut2001@gmail.com>
2024-03-08 15:16:25 +0300

    lesson 8 start

---
+++ Lesson 8 - FileSystem/Task 8.1 - GetAllDirectories/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task_8._1___GetAllDirectories
+{
+    /*Обязательная задача*. Выберите любую папку на своем компьютере, имеющую вложенные директории.
+     Выведите на консоль ее содержимое и содержимое всех подкаталогов.*/
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"D:For"
+        }
+    }
+}
---
+++ Lesson 8 - FileSystem/Task 8.3 - GetTxtFileStatistics/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task_8._3___GetTxtFileStatistics
+{
+    /*Вручную подготовьте текстовый файл с фрагментом текста.
+     Напишите программу, которая выводит статистику по файлу - количество символов, строк и слов в тексте.*/
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
