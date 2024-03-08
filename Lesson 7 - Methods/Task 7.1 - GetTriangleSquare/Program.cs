a972cc950006708deea25a6a100323dcf3ede04d
Davut Sukhankuliev <stalker.davut2001@gmail.com>
2024-03-08 15:16:01 +0300

    lesson 7 complete

---
+++ Lesson 7 - Methods/Task 7.1 - GetTriangleSquare/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task_7._1___GetTriangleSquare
+{
+    /*Обязательная задача*. Два треугольника заданы длинами своих сторон.
+     Определить, площадь какого из них больше 
+     (создать метод для вычисления  площади  треугольника по длинам его сторон).  
+     Для решения задачи можно использовать формулу Герона */
+    
+    internal class Program
+    {
+        private static double CalculateTriangleSquare(double a, double b, double c)
+        {
+            double halfPerimeter = CalculatePerimeter(a, b, c) / 2;
+
+            var square = Math.Sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c));
+            
+            return square;
+        }
+        
+        private static double CalculatePerimeter(params double[] sides)
+        {
+            double s = 0;
+            
+            foreach (var side in sides)
+            {
+                s += side;
+            }
+
+            return s;
+        }
+        
+        public static void Main(string[] args)
+        {
+            var a = Convert.ToDouble(Console.ReadLine());
+            var b = Convert.ToDouble(Console.ReadLine());
+            var c = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"{CalculateTriangleSquare(a, b, c)}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson 7 - Methods/Task 7.2 - GetCubeVolume/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task_7._2___GetCubeVolume
+{
+    /*Создать метод для вычисления объема и площади поверхности куба по длине его ребра*/
+    
+    internal class Program
+    {
+        private static void CalculateCubeVolumeAndArea(ref double side, out double volume, out double area)
+        {
+            area = 0;
+            volume = 0;
+            
+            if (side < 0)
+            {
+                Console.WriteLine($"Была введена сторона с отрицательной длиной {side}" +
+                                  $"\r\nМетод отработал через модуль");
+                side = -side;
+            }
+            else if (side == 0)
+            {
+                Console.WriteLine($"Была введена сторона с отрицательной длиной {side}" +
+                                  $"\r\nМетод отработал через модуль");
+                return;
+            }
+            
+            area = side * side * 6;
+            volume = side * side * side;
+        }
+        
+        public static void Main(string[] args)
+        {
+            var side = Convert.ToDouble(Console.ReadLine());
+
+            CalculateCubeVolumeAndArea(ref side, out var volume, out var area);
+            
+            Console.WriteLine($"Длина ребра куба: \t{side}" +
+                              $"\r\nПлощадь поверхности куба: \t{area}" +
+                              $"\r\nОбъем куба: \t{volume}");
+            Console.ReadKey();
+        }
+    }
+}
