79fed6eec96975a045ef5cb7871204ed8df833a9
Davut Sukhankuliev <stalker.davut2001@gmail.com>
2024-03-08 15:30:58 +0300

    8.1 complete

--- Lesson 8 - FileSystem/Task 8.1 - GetAllDirectories/Program.cs
+++ Lesson 8 - FileSystem/Task 8.1 - GetAllDirectories/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Task_8._1___GetAllDirectories
+﻿using System;
+using System.IO;
+
+namespace Task_8._1___GetAllDirectories
 {
     /*Обязательная задача*. Выберите любую папку на своем компьютере, имеющую вложенные директории.
      Выведите на консоль ее содержимое и содержимое всех подкаталогов.*/
@@ -7,7 +10,15 @@
     {
         public static void Main(string[] args)
         {
-            string path = @"D:For"
+            string path = @"D:\Davut\University\Master ITMO\2 semester\CsharpBasis\Lesson 8 - FileSystem";
+            string[] files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            Console.ReadKey();
         }
     }
 }
